7972ef12ad62685e59b87c401b61322b92b11053
Brian Kwon <bkwon94@gmail.com>
2019-05-25 18:12:36 -0400

    Setup basic flexbox properties for app

--- client/src/components/CurrentPlayer.jsx
+++ client/src/components/CurrentPlayer.jsx
@@ -4,9 +4,11 @@ const CurrentPlayer = () => {
   return (
     <div>
       <h1>Pool Leaderboard</h1>
-      <h4>PLACE</h4>
-      <h4>IMAGE</h4>
-      <h4>WINS</h4>
+      <div className="player-info">
+        <h4>PLACE</h4>
+        <h4>IMAGE</h4>
+        <h4>WINS</h4>
+      </div>
     </div>
   )
 }
