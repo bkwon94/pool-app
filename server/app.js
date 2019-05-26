eb9cdb24bb1410340959efd7a16fa415fc433af6
Brian Kwon <bkwon94@gmail.com>
2019-05-25 22:29:15 -0400

    Add get route to retrieve all players

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('../database/index.js');
 
 const app = express();
 
@@ -8,5 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../public')));
 
+app.get('/players', async(req, res) => {
+  let players = await db.getPlayers();
+  res.send(players);
+});
 
 module.exports = app;
