684fe9fea5c30044628b1db0a201e87236d39902
Brian Kwon <bkwon94@gmail.com>
2019-05-28 02:32:33 -0400

    Add create player functionality

--- client/src/components/ChooseOption.jsx
+++ client/src/components/ChooseOption.jsx
@@ -5,17 +5,31 @@ const ChooseOption = () => {
 
   const [newGame, setNewGame] = useState(false);
   const [addPlayer, setAddPlayer] = useState(false);
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('/players', {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        name: name,
+        image: 'https://s3.amazonaws.com/uifaces/faces/twitter/haligaliharun/128.jpg',
+        wins: 0
+      })
+    });
+  }
 
   if (!newGame && !addPlayer) {
     return (
-      <div id="options">
+      <div className="options">
         <div className="options-button" onClick={() => setNewGame(true)}>New Game</div>
         <div className="options-button" onClick={() => setAddPlayer(true)}>Add Player</div>
       </div>
     )
   } else if (newGame && !addPlayer) {
     return (
-      <div>
+      <div className="options">
         <ul>
           <li>yo</li>
         </ul>
@@ -26,8 +40,11 @@ const ChooseOption = () => {
     )
   } else {
     return (
-      <div>
-        ADD NEW PLAYER
+      <div className="options">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={name} onChange={event => setName(event.target.value)} placeholder="Enter name..."/>
+          <input type="submit" value="Create"/>
+        </form>
       </div>
     )
   }
--- server/app.js
+++ server/app.js
@@ -11,8 +11,14 @@ app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/players', async(req, res) => {
   let players = await db.getPlayers();
-  console.log('players:', players);
   await res.send(players);
+});
+
+app.post('/players', async(req, res) => {
+  console.log(req.body);
+  let player = req.body;
+  db.insertPlayer(player);
+  await res.send('Done');
 });
 
 module.exports = app;
