7500bf6578a17df25210dd0e6e875e3a6c4b0172
Brian Kwon <bkwon94@gmail.com>
2019-05-28 01:52:23 -0400

    Add underline hover effect for buttons and change main color theme of app

--- client/src/components/ChooseOption.jsx
+++ client/src/components/ChooseOption.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 
 const ChooseOption = ({ winnerClick }) => {
+
   return (
     <div id="options">
-      <div className="options-button">New Game</div>
+      <div className="options-button" onClick={() => openModal(true)}>New Game</div>
       <div className="options-button">Add Player</div>
-      <div className="options-button">Delete Player</div>
     </div>
   )
 }
